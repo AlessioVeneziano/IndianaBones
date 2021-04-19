a6d17197237efd0e2186fcc80ac47dfe8374ec98
Alessio Veneziano <71142703+AlessioVeneziano@users.noreply.github.com>
2021-04-19 19:45:11 +0100

    Update exampleStack.R

--- R/exampleStack.R
+++ R/exampleStack.R
@@ -1,6 +1,7 @@
 #' @title example 3D image stack
 #' @description Binarized 3D image stack of the mandibular condyle of Hylobates lar (specimen MCZ:Mamm:41421 in morphosource.org)
 #' @name exampleStack
+#' @aliases exampleStack
 #' @docType data
 #' @author Alessio Veneziano
 #' @keywords indianaBones
