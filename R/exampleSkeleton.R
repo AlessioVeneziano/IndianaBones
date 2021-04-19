7355a94984713a37763dd5933f7baab8d04aad44
Alessio Veneziano <71142703+AlessioVeneziano@users.noreply.github.com>
2021-04-19 19:44:50 +0100

    Update exampleSkeleton.R

--- R/exampleSkeleton.R
+++ R/exampleSkeleton.R
@@ -1,6 +1,7 @@
 #' @title example 3D topological skeleton
 #' @description 3D topological skeleton of the cancellous bone of the mandibular condyle of Hylobates lar (specimen MCZ:Mamm:41421 in morphosource.org). The cancellous bone was extracted using the function \code{splitBone}. The skeleton was calculated using the Amira software (FEI visualization).
 #' @name exampleSkeleton
+#' @aliases exampleSkeleton
 #' @docType data
 #' @author Alessio Veneziano
 #' @keywords indianaBones
